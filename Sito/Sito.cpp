446b104ad3f98aa76f040e56bfa0cd2788107d8d
Yahor <egorpotapko@mail.ru>
2020-03-20 20:45:32 +0100

    Sift update

--- Sito/Sito.cpp
+++ Sito/Sito.cpp
@@ -3,11 +3,33 @@
 using namespace std;
 
 Sito::Sito(int size) {
-  this->size = size;
+    try{
+        if(size<2){
+            throw "Size must be >1";
+        }
+        this->size=size;
+    }
+    catch(const char* str) {
+        cerr<<"Error. "<<str;
+        exit(0);
+    }
+
+}
+
+void Sito::fillDenominator() {
+    denominator[0]=2;
+    denominator[1]=3;
+    denominator[2]=5;
+    denominator[3]=7;
 }
 
 void Sito::Sift() {
+    for(int i=2; i<size+1; i++){
+        allDigits.push_back(i);
+    }
+    for(int i=0; i<allDigits.size(); i++){
 
+    }
 }
 
 void Sito::Display() {
@@ -19,9 +41,10 @@ void Sito::Check(int x) {
 }
 
 void Sito::DoActions() {
-Sift();
-Display();
-Check(7);
+    fillDenominator();
+    Sift();
+    Display();
+    Check(7);
 }
 
 int Sito::getSize() const {

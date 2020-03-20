5abce8d3c4db61c5252b9443a04b0194a3dbacb5
Yahor <egorpotapko@mail.ru>
2020-03-20 22:20:40 +0100

    full interface 1.0

--- Sito/Sito.h
+++ Sito/Sito.h
@@ -7,19 +7,22 @@ class Sito {
 private:
     int size;
     bool sizeIsTrue = false;
-    std::vector<int> arr;
+    std::vector<int> allDigits;
+    std::vector<int> sortedDigits;
+    int denominator[4];
 public:
     int getSize() const;
     void setSize(int size);
 
     explicit Sito(int size);
+    void fillDenominator();
     void Sift();   // Odsiej
-    void Display();
+    void DisplaySifted();
     void Check(int x);
     void DoActions();
 
 //    static void Sift();   // Odsiej
-//    static void Display();
+//    static void DisplaySifted();
 //    static void Check(int x);
 //    static void DoActions();
 };
